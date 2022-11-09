3c5b1a88266b7ae3b6dc20409a69abff56f33d12
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2022-11-09 18:00:42 +0100

    Add option to restart and shutdown computer in Mac

--- src/entities/commandMac.js
+++ src/entities/commandMac.js
@@ -1,6 +1,6 @@
 module.exports = {
     chrome: 'open -a "Google Chrome" https://www.google.com/',
     vscode: "code .",
-    shutdown: "",
-    restart: ""
+    shutdown: "sudo shutdown --p ",
+    restart: "sudo shutdown -r now"
 }
