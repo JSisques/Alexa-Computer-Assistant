e7903dec4669fd77790046af97e63986c9e4537f
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2022-11-09 17:41:10 +0100

    Update send request to execute a command

---
+++ src/entities/commandMac.js
@@ -0,0 +1,6 @@
+module.exports = {
+    chrome: 'open -a "Google Chrome" https://www.google.com/',
+    vscode: "code .",
+    shutdown: "",
+    restart: ""
+}
--- src/exec/controller.js
+++ src/exec/controller.js
@@ -1,15 +1,16 @@
 const model = require('./model')
 const logger = require("../util/logger")
+const command = require("../entities/commandMac")
 
 module.exports = {
 
    async execCommand(req, res) {
 
-       var command = req.params.COMMAND
-       command = process.env.COMMAND_MAC_CHROME
-       logger.info(`Comando de la petición: "` + command + `"`)
+       var reqCommand = req.params.COMMAND
+       var reqCommand = command[reqCommand]
+       logger.info(`Comando de la petición: "` + reqCommand + `"`)
 
-       var result = await model.execCommand(command)
+       var result = await model.execCommand(reqCommand)
        console.log("Resultado: " + result.stdout)
 
        return res.send(result)
